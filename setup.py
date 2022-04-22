b75a5088cd505ebf39ca2ab4a7bb2b43112e0e48
Axel Vanraes <axelvanraes@gmail.com>
2022-04-22 15:59:30 +0200

    Update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ classifiers = [
 ]
 setup(
     name="FHIRkit",
-    version="0.0.6",
+    version="0.0.7",
     description="Toolkit to handle FHIR Resources in a more efficient, pythonic way.",
     long_description=open("README.md").read(),
     url="",
