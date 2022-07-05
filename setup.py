b77fb3e673b12d2f172f2ba5233f53265bbd9dcd
Axel Vanraes <axelvanraes@gmail.com>
2022-07-05 15:47:40 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ classifiers = [
 ]
 setup(
     name="FHIRkit",
-    version="0.0.18",
+    version="0.0.19",
     description="Toolkit to handle FHIR Resources in a more efficient, pythonic way.",
     long_description=open("README.md").read(),
     url="",
