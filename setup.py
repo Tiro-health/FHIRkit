278f46144ebc4da109379e87d9385553ffa64e7c
Axel Vanraes <axel.vanraes@tiro.health>
2023-02-16 15:00:09 +0100

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ classifiers = [
 ]
 setup(
     name="FHIRkit",
-    version="0.0.33",
+    version="0.0.35",
     description="Toolkit to handle FHIR Resources in a more efficient, pythonic way.",
     long_description=open("README.md").read(),
     url="",
