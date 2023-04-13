82e6d7161f1836d1086572e502bcc59b541ea202
devliegherec <charlottedevlieghere@gmail.com>
2023-04-13 09:14:21 +0200

    Bumped version to 0.0.43

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ classifiers = [
 ]
 setup(
     name="FHIRkit",
-    version="0.0.42",
+    version="0.0.43",
     description="Toolkit to handle FHIR Resources in a more efficient, pythonic way.",
     long_description=open("README.md").read(),
     url="",
